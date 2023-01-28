3a5020ccf773105907feca0adf9482a80b7d6ff5
dhvani3289 <123374029+dhvani3289@users.noreply.github.com>
2023-01-28 17:14:13 +0530

    Add files via upload

---
+++ proQ3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int c,f;
+
+	printf("enter celsius value  : ");
+	scanf("%d",&c);
+	
+	f=1.8*c+32;
+	printf("Temperature in fahrenheit is:%d\n",f);	
+	
+	
+}
+	
+	
+	
+	
